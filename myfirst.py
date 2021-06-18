33527a2f081cc75cfc274a6cf3c51daba4a60cc6
Sthephanie Giraldo <sthephanieg95@gmail.com>
2021-06-17 21:47:06 -0500

    excepciones

--- myfirst.py
+++ myfirst.py
@@ -1,9 +1,15 @@
-horas=float(input("Introduzca las horas:  \n"))
-tarifa=float(input("introduzca la tarifa por hora:  \n"))
-if horas > 40:
-    r1=tarifa*horas
-    r2=(horas-40)*(tarifa*0.5)
-    rf=r1+r2
-else:
-    rf=tarifa*horas
-print("resultado: " + str(rf))
+try:
+    horas=float(input("Introduzca las horas:  \n"))
+    tarifa=float(input("introduzca la tarifa por hora:  \n"))
+    if horas > 40:
+        r1=tarifa*horas
+        r2=(horas-40)*(tarifa*0.5)
+        rf=r1+r2
+    else:
+        rf=tarifa*horas
+    print("resultado: " + str(rf))
+except:
+    print("Introduzca un numero")
+    
+
+
